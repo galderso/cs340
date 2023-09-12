d3f08d22a72511fd05a3472600a608de986a16da
Makenzie Johnson <mvolleyballjohnson@gmail.com>
2023-09-12 19:22:29 -0400

    added my file

--- helloplus/mjohn326/README.md
+++ helloplus/mjohn326/README.md
@@ -1,1 +1,2 @@
-The program will do something :)
+This tracker will list the activites you must complete. Once finished, you can check off the item.
+You can add and remove to the activities list if you want
---
+++ helloplus/mjohn326/tracker.py
@@ -0,0 +1,8 @@
+activities = ["CS Homework", "Exercise", "Drink Water"]
+
+while True:
+    user_input = input("Commands:\n 'q' = quit\n 's' = show activity list\n 'a' = add to activity list\n 'r' = remove from activity list\n")
+    
+    if user_input == 'q':
+        break  # Exit the loop if the user enters 'q'
+
