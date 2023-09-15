24ce89bcae94fbd59575d085b3ef499d8f855665
Makenzie Johnson <mvolleyballjohnson@gmail.com>
2023-09-15 08:10:06 -0400

    Deleted files

--- helloplus/mjohn326/README.md
+++
@@ -1,2 +0,0 @@
-This tracker will list the activites you must complete. Once finished, you can check off the item.
-You can add and remove to the activities list if you want
--- helloplus/mjohn326/tracker.py
+++
@@ -1,8 +0,0 @@
-activities = ["CS Homework", "Exercise", "Drink Water"]
-
-while True:
-    user_input = input("Commands:\n 'q' = quit\n 's' = show activity list\n 'a' = add to activity list\n 'r' = remove from activity list\n")
-    
-    if user_input == 'q':
-        break  # Exit the loop if the user enters 'q'
-
